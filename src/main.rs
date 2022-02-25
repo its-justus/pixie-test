ec42d5cdbb35dd8210fef699cb8a4f7906643521
Ian Johnson <ianjohnson9042@gmail.com>
2022-02-24 22:39:06 -0600

    added post user route

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,21 @@
 use std::net::SocketAddr;
 
-use axum::{routing::{get},
+use axum::{routing::{get, post},
 	http::StatusCode,
 	response::IntoResponse,
 	Json, Router,};
+use serde::{Deserialize, Serialize};
 
 #[tokio::main]
 async fn main() {
+	tracing_subscriber::fmt::init();
+
 	let app = Router::new()
-		.route("/", get(root));
+		.route("/", get(root))
+		.route("/register", post(register));
 	
 	let addr = SocketAddr::from(([127,0,0,1], 3000));
-	println!("listening on {}", addr);
+	tracing::debug!("listening on {}", addr);
 	axum::Server::bind(&addr)
 		.serve(app.into_make_service())
 		.await
@@ -20,4 +24,24 @@ async fn main() {
 
 async fn root() -> &'static str{
 	"Hello, world!"
+}
+
+
+async fn register(Json(payload): Json<CreateUser>) -> impl IntoResponse {
+	let user = User{
+		id: 42,
+		username: payload.username,
+	};
+	(StatusCode::CREATED, Json(user))
+}
+
+#[derive(Deserialize)]
+struct CreateUser {
+	username: String,
+}
+
+#[derive(Serialize)]
+struct User {
+	id: usize,
+	username: String,
 }
