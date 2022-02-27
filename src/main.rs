3c85645b6c7e74d0fa844f3cabb388224ef57862
Ian Johnson <ianjohnson9024@gmail.com>
2022-02-27 01:02:58 -0600

    trying to get query result into User type for returning to front end

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,34 @@
 use std::net::SocketAddr;
 
 use axum::{
+    async_trait,
+    extract::{Extension, FromRequest, RequestParts},
     http::Method,
     http::StatusCode,
     response::IntoResponse,
     routing::{get, post},
-    Json, Router,
+    AddExtensionLayer, Json, Router,
 };
 use serde::{Deserialize, Serialize};
 use tower_http::cors::{Any, CorsLayer, Origin};
 
+use bb8::{Pool, PooledConnection};
+use bb8_postgres::PostgresConnectionManager;
+use tokio_postgres::{Error, NoTls};
+
 #[tokio::main]
 async fn main() {
+    // Set the RUST_LOG, if it hasn't been explicitly defined
+    if std::env::var_os("RUST_LOG").is_none() {
+        std::env::set_var("RUST_LOG", "example_tokio_postgres=debug")
+    }
     tracing_subscriber::fmt::init();
+
+    // setup postgres connection manager
+    let manager =
+        PostgresConnectionManager::new_from_stringlike("hostname=localhost user=pixietest", NoTls)
+            .unwrap();
+    let pool = Pool::builder().build(manager).await.unwrap();
 
     // creates cors policy
     let cors_layer = CorsLayer::new()
@@ -25,7 +41,8 @@ async fn main() {
         .route("/", get(root))
         .route("/register", post(register))
         .route("/campaign", post(create_campaign))
-        .layer(cors_layer);
+        .layer(cors_layer)
+        .layer(AddExtensionLayer::new(pool));
 
     // binds app to a socket and starts the application
     let addr = SocketAddr::from(([127, 0, 0, 1], 9001));
@@ -37,18 +54,31 @@ async fn main() {
         .unwrap();
 }
 
+type ConnectionPool = Pool<PostgresConnectionManager<NoTls>>;
+
 async fn root() -> &'static str {
     "Hello, world!"
 }
 
 // TODO: add Discord OAuth2 support
-async fn register(Json(payload): Json<CreateUser>) -> impl IntoResponse {
-    // uses serde_json to deserialize the request payload and returns a json response
-    let user = User {
-        id: 42,
-        username: payload.username,
-    };
-    (StatusCode::CREATED, Json(user))
+async fn register(
+    Json(payload): Json<CreateUser>,
+    Extension(pool): Extension<ConnectionPool>,
+) -> impl IntoResponse {
+    // get a connection from the pool
+    let conn = pool.get().await.map_err(internal_error)?;
+    let row = conn
+        .query_one(
+            "insert into users (name) values ($1) returning id, name",
+            &[&payload.username],
+        )
+        .await
+        .map_err(internal_error)?;
+    row.try_get::<usize, User>(0).map_err(internal_error)
+    /*
+    Need to figure out how to get the row to go into the User type.
+    not sure how to do this, but I assume it's possible
+    */
 }
 
 async fn create_campaign(Json(payload): Json<CreateCampaign>) -> impl IntoResponse {
@@ -82,4 +112,13 @@ struct Campaign {
     id: usize,
     name: String,
     testers: usize,
+}
+
+/// Utility function for mapping any error into a `500 Internal Server Error`
+/// response.
+fn internal_error<E>(err: E) -> (StatusCode, String)
+where
+    E: std::error::Error,
+{
+    (StatusCode::INTERNAL_SERVER_ERROR, err.to_string())
 }
